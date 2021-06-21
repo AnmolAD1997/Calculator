f14127bc6a2e7c4f3cc3379eb46329bde52a9849
AnmolAD1997 <64797943+AnmolAD1997@users.noreply.github.com>
2021-06-21 16:35:52 -0500

    Calculator commit

---
+++ src/main/java/com/example/controller/CalcController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class CalcController {
+
+	@GetMapping("/sum")
+	String calc() {
+		return "The sum is 100";
+	}
+	
+	
+}
